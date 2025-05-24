4d9ef7a23a5c17fbc9092bc3f4e995d510c69f9b
firassattout <firas.sattout@gmail.com>
2025-05-24 14:43:50 +0300

    add publisher v

--- routes/old/index.js
+++
@@ -1,7 +0,0 @@
-import { auth } from "./auth.mjs";
-import { groups } from "./groups.mjs";
-import { user } from "./user.mjs";
-import { fileRoutes } from "./fileRoutes.mjs";
-import { tracing } from "./tracing.mjs";
-
-export { auth, groups, user, fileRoutes, tracing };
