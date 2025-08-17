9eff8cf48b778574daa7a3d377efae8b12c348ab
firassattout <firas.sattout@gmail.com>
2025-08-17 19:03:41 +0300

    test4

--- ExternalApiService.mjs
+++ ExternalApiService.mjs
@@ -154,7 +154,7 @@ async function getTransactionByProgram(data) {
     if (error.response) {
       throw new Error(error.response.data);
     } else if (error.request) {
-      throw new Error(error.request);
+      throw new Error(error.request.data);
     } else {
       throw new Error(error.message);
     }
